10adfe8597308a5257b1431269f0a49ee3bcd6b9
barkode <konstantinks@gmail.com>
2023-06-27 12:23:11 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,39 @@
-import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  Platform,
-  ImageBackground,
-} from "react-native";
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import imageBG from "./images/BG.jpg";
+// import React from "react";
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   View,
+//   Text,
+//   Platform,
+//   ImageBackground,
+// } from "react-native";
+// import RegistrationScreen from "./Screens/RegistrationScreen";
+// import imageBG from "./images/BG.jpg";
+// import { Button } from "react-native-web";
 
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.container}>
-          <ImageBackground source={imageBG} style={styles.imageBG}>
-            <RegistrationScreen />
-          </ImageBackground>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
-};
+// const App = () => {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <View style={styles.container}>
+//         <View style={styles.container}>
+//           <ImageBackground source={imageBG} style={styles.imageBG}>
+//             <RegistrationScreen />
+//           </ImageBackground>
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//   );
+// };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    zIndex: -1,
-  },
-  imageBG: {
-    flex: 1,
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     zIndex: -1,
+//   },
+//   imageBG: {
+//     flex: 1,
+//     justifyContent: "center",
+//   },
+// });
 
-export default App;
+// export default App;
