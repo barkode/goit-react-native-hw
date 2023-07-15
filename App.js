faac32ce880714067af8428af7491e463b9df958
barkode <konstantinks@gmail.com>
2023-07-15 23:15:26 +0100

    add files hw2

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { useFonts } from "expo-font";
+import RegistrationScreen from "./screens/RegistrationScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
+  const [screen, setScreen] = useState("RegistrationScreen");
+  const [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        {screen === "RegistrationScreen" ? (
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.keyboardAvoidingView}>
+            <RegistrationScreen setScreen={setScreen} />
+          </KeyboardAvoidingView>
+        ) : (
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.keyboardAvoidingView}>
+            <LoginScreen setScreen={setScreen} />
+          </KeyboardAvoidingView>
+        )}
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  keyboardAvoidingView: {
+    flex: 1,
   },
 });
+
+// import { useState } from "react";
+// import { View, StyleSheet } from "react-native";
+// import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { StatusBar } from "expo-status-bar";
+// import { useFonts } from "expo-font";
+// import RegistrationScreen from "./screens/RegistrationScreen";
+// import LoginScreen from "./screens/LoginScreen";
+
+// export default function App() {
+//   useFonts({
+//     "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+//     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+//     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+//   });
+
+//   const [screen, setScreen] = useState("RegistrationScreen");
+
+//   return (
+//     <SafeAreaProvider>
+//       <View style={styles.container}>
+//         {screen === "RegistrationScreen" ? (
+//           <RegistrationScreen setScreen={setScreen} />
+//         ) : (
+//           <LoginScreen setScreen={setScreen} />
+//         )}
+//         <StatusBar style="auto" />
+//       </View>
+//     </SafeAreaProvider>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//   },
+// });
+/////////////////////////////*********///////////////////////////////////////////**** */ */
+// import { useFonts } from "expo-font";
+// import React from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+
+// import RegistrationScreen from "./screens/RegistrationScreen";
+// import LoginScreen from "./screens/LoginScreen";
+// import { ImageBackground } from "react-native";
+
+// const Stack = createStackNavigator();
+
+// export default function App() {
+//   const [fontsLoaded] = useFonts({
+//     "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+//     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+//     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+//   });
+
+//   if (!fontsLoaded) {
+//     return null;
+//   }
+
+//   return (
+//     <ImageBackground style={{flex:1, resizeMode:"cover"}}
+//       source={require("./images/PhotoBG.png")}
+//       >
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Registration" component={RegistrationScreen} />
+//         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//     </ImageBackground>
+//   );
+// }
---
+++ screens/LoginScreen.jsx
@@ -0,0 +1,186 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Image,
+} from "react-native";
+
+const LoginScreen = ({ setScreen }) => {
+  const [isInputActive, setIsInputActive] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onLogin = () => {
+    console.log("email:", email);
+    console.log("password:", password);
+  };
+
+  const onInputActive = (input) => {
+    setIsInputActive(input);
+  };
+  const onInputBlur = () => {
+    setIsInputActive("");
+  };
+
+  const handlePressOutside = () => {
+    Keyboard.dismiss();
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={handlePressOutside}>
+      <View style={styles.container}>
+        <Image
+          source={require("../images/PhotoBG.png")}
+          resizeMode="cover"
+          style={styles.image}
+        />
+        <View style={styles.form}>
+          <Text style={styles.title}>Увійти</Text>
+          <View style={styles.inputsList}>
+            <TextInput
+              onChangeText={setEmail}
+              value={email}
+              onFocus={() => onInputActive("email")}
+              onBlur={onInputBlur}
+              style={[
+                styles.input,
+                isInputActive === "email" && styles.inputActive,
+              ]}
+              placeholder="Адреса електронної пошти"
+            />
+            <TextInput
+              onChangeText={setPassword}
+              value={password}
+              onFocus={() => onInputActive("password")}
+              onBlur={onInputBlur}
+              style={[
+                styles.input,
+                isInputActive === "password" && styles.inputActive,
+              ]}
+              placeholder="Пароль"
+            />
+          </View>
+          <TouchableOpacity onPress={onLogin} style={styles.btnRegister}>
+            <Text style={styles.textRegister}>Увійти</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => setScreen("RegistrationScreen")}>
+            <Text style={styles.textLogin}>
+              Немає акаунту?{" "}
+              <Text style={styles.textLoginLink}>Зареєструватися</Text>
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "100%",
+  },
+
+  image: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+  },
+
+  form: {
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    marginTop: "auto",
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingTop: 32,
+  },
+
+  title: {
+    fontWeight: 500,
+    fontSize: 30,
+    fontFamily: "Roboto-Medium",
+    lineHeight: 35,
+    textAlign: "center",
+    color: "#212121",
+    marginBottom: 32,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    borderStyle: "solid",
+    backgroundColor: "#F6F6F6",
+    padding: 15,
+    maxHeight: 50,
+    marginBottom: 16,
+
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    lineHeight: 19,
+  },
+
+  inputActive: {
+    borderWidth: 1,
+    borderColor: "#FF6C00",
+    borderRadius: 8,
+    borderStyle: "solid",
+    backgroundColor: "#FFFFFF",
+    padding: 15,
+    maxHeight: 50,
+    marginBottom: 16,
+    color: "#212121",
+
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    lineHeight: 19,
+  },
+
+  inputsList: {
+    marginBottom: 43,
+  },
+
+  btnRegister: {
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    width: "100%",
+    height: 51,
+    marginBottom: 16,
+  },
+
+  textRegister: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    lineHeight: 19,
+  },
+
+  textLogin: {
+    color: "#1B4371",
+    textAlign: "center",
+    alignItems: "baseline",
+    marginBottom: 45,
+  },
+
+  textLoginLink: {
+    color: "#1B4371",
+    textDecorationLine: "underline",
+  },
+});
+
+export default LoginScreen;
