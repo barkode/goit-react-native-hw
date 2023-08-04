6fcb893549cb5f7410e758a64ebe886e1e0c3f15
barkode <konstantinks@gmail.com>
2023-08-04 16:57:25 +0100

    Create icons.js

---
+++ utils/icons.js
@@ -0,0 +1,40 @@
+export const addIcon = () => {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12.5" cy="12.5" r="12" fill="white" stroke="#FF6C00" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M13 6H12V12H6V13H12V19H13V13H19V12H13V6Z"
+        fill="#FF6C00"
+      />
+    </svg>
+  );
+};
+
+export const userIcon = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M28 29V27C28 24.7909 26.2091 23 24 23H16C13.7909 23 12 24.7909 12 27V29"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        clip-rule="evenodd"
+        d="M20 19C22.2091 19 24 17.2091 24 15C24 12.7909 22.2091 11 20 11C17.7909 11 16 12.7909 16 15C16 17.2091 17.7909 19 20 19Z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
