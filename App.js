9ea99dee147e8e22f6cf0a1b81d7a8922439f045
barkode <konstantinks@gmail.com>
2023-08-06 19:52:56 +0100

    change file names

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { Provider } from "react-redux";
 
 import { store } from "./redux/store";
 
-import { IndexComponent } from "./components/IndexComponent";
+import { Main } from "./components/Main";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <IndexComponent></IndexComponent>
+      <Main></Main>
     </Provider>
   );
 }
